9133e3d0544da60a1fbd3997400bdeca82756717
ciyana273 <ciyana2731276@gmail.com>
2025-10-15 15:53:57 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,7 @@
+list1 = [8, 6, 7, 4, 5]
+list2 = [5, 4, 3, 2, 1]
+if len(list1) == len(list2):
+  print("a. The lists have the same length.")
+else:
+  print("a. The lists have different lengths.")
+  print(f"b. sum of list1: {sum(list1)} & sum of list2: {sum(list2)}")
