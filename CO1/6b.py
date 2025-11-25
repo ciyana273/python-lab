d2ca643782a7ef26b6b166109bcf65f26b7f915a
ciyana273 <ciyana2731276@gmail.com>
2025-11-25 11:34:36 +0530

    Add files via upload

---
+++ CO1/6b.py
@@ -0,0 +1,7 @@
+list1 = [8, 6, 7, 4, 5]
+list2 = [5, 4, 3, 2, 1]
+print(f"b. sum of list1: {sum(list1)} & sum of list2: {sum(list2)}")
+if sum(list1) == sum(list2):
+ print("   The lists sum to the same value.")
+else:
+ print("   The lists do not sum to the same value.")
